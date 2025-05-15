3eb8b402a26f0c116b3f5b5da3f2e3cfa3a10948
makebacappiello <makebacappiello@gmail.com>
2025-05-15 13:11:52 -0400

    added notes to home.jsx

--- version-1/src/pages/SavedCountries.jsx
+++ version-1/src/pages/SavedCountries.jsx
@@ -21,6 +21,12 @@ export default function SavedCountries() {
   function handleSubmit(event) {
     event.preventDefault();
     console.log("FORM DaTA", formData);
+    setFormData({
+      name: "",
+      emailAddress: "",
+      country: "",
+      bio: "",
+    });
   }
   return (
     <>
@@ -28,49 +34,55 @@ export default function SavedCountries() {
         <h1>MY Saved Countries</h1>
         <CountryDetail />
         <h1>My Profile</h1>
-        <div>
-          <label htmlFor="name">Full Name</label>
-          <input
-            className="input"
-            type="text"
-            name="name"
-            id="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="emailAddress"> Email</label>
-          <input
-            type="email"
-            name="emailAddress"
-            id="emailAddress"
-            value={formData.emailAddress}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="country">Country</label>
-          <input
-            type="text"
-            name="country"
-            id="country"
-            value={formData.country}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="bio">Bio</label>
-          <textarea
-            rows="8"
-            cols="50"
-            type="text"
-            name="bio"
-            id="bio"
-            value={formData.bio}
-            onChange={handleChange}
-          ></textarea>
-        </div>
+        <form className="form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="emailAddress"> Email</label>
+            <input
+              type="email"
+              name="emailAddress"
+              id="emailAddress"
+              value={formData.emailAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="country">Country</label>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              value={formData.country}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="bio">Bio</label>
+            <textarea
+              rows="8"
+              cols="50"
+              type="text"
+              name="bio"
+              id="bio"
+              value={formData.bio}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <div>
+            <button className="button" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
