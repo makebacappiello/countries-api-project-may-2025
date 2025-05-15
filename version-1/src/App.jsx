654618a776c5300b633ea3b1d7c882f706b46c83
makebacappiello <makebacappiello@gmail.com>
2025-05-15 18:52:28 -0400

    changed router to app.jsx file

--- version-1/src/App.jsx
+++ version-1/src/App.jsx
@@ -2,15 +2,32 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import SavedCountries from "./pages/SavedCountries";
 import CountryDetail from "./pages/CountryDetail.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [apiData, setApiData] useState([]);
+  
+  useEffect(() => {
+    const getApiData = async () => {
+      try {
+        const response = await fetch ("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setApiData(data);
+      } catch (error) { 
+        console.error("Oops! Error fetching data:", error);
+      }
+    };
+
+    getApiData();
+  }, []);
+
+  
   return (
     <div>
       <nav className="navbar">
         <ul className="navLinks">
           <li>
             <Link to="/">Where in the world?</Link>
-            {/* <Link to="/">Home</Link> */}
           </li>
           <li>
             <Link to="/SavedCountries">Saved Countries</Link>
@@ -18,11 +35,11 @@ function App() {
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/SavedCountries" element={<SavedCountries />} />
+        <Route path="/" element={<Home apiData={apiData}/>} />
+        <Route path="/SavedCountries" element={<SavedCountries apiData={apiData}/>} />
         <Route
           path="/country-detail/:countryName"
-          element={<CountryDetail />}
+          element={<CountryDetail apiData={apiData}/>}
         />
       </Routes>
     </div>
--- version-1/src/pages/CountryDetail.jsx
+++ version-1/src/pages/CountryDetail.jsx
@@ -4,25 +4,26 @@ import { Link } from "react-router-dom";
 
 export default function CountryDetail(
   {
-    //   key,
-    //   name,
-    //   population,
-    //   region,
-    //   capital,
-    //   img,
+      key,
+      name,
+     population,
+     region,
+     capital,
+     img,
   }
 ) {
   return (
     <Link to={`country-detail/${item.name.common}`}>
       <div className="card">
         <h1>Country Detail</h1>
-        {/* <img src={img} />
+         <img src={img} />
       <p>COUNTRY: {name}</p>
       <p>POPULATION: {population}</p>
       <p>REGION:{region}</p>
       <p>CAPITAL:{capital}</p>
-    </div> */}
+    </div> 
       </div>
+      
     </Link>
   );
 }
