f3d64ed98a5b113ba74ae9e91c2cce401dc9e5cf
makebacappiello <makebacappiello@gmail.com>
2025-05-16 15:50:24 -0400

    completed styling the saved countries page

--- version-1/src/pages/CountryDetail.jsx
+++ version-1/src/pages/CountryDetail.jsx
@@ -6,20 +6,30 @@ import { useParams } from "react-router-dom";
 export default function CountryDetail({ apiData }) {
   const { countryName } = useParams();
   const country = apiData.find(
-    (c) => c.name.common.toLowerCase() === countryName.toLocaleLowerCase()
+    (c) => c.name.common.toLowerCase() === countryName.toLowerCase()
   );
-  if (!country) return;
-  <p>Loading Country Data...Possibility of Apples and Rain...</p>;
-  return (
-    <Link to={`country-detail/${item.name.common}`}>
-      <div className="card">
-        <h1>Country Detail</h1>
-        <img src={img} />
-        <p>COUNTRY: {name}</p>
-        <p>POPULATION: {population}</p>
-        <p>REGION:{region}</p>
-        <p>CAPITAL:{capital}</p>
+  if (!country) {
+    return (
+      <div className="country-detail">
+        <p>Loading Country Data...Possibility of Apples and Rain...</p>;
       </div>
-    </Link>
+    );
+  }
+  return (
+    <div className="country-detail">
+      <Link
+        to="/"
+        // {`country-detail/${country.name.common}`}
+      >
+        <div className="card">
+          <h1>Country Detail</h1>
+          <img src={country.flags.png} alt={`${country.name.common} flag`} />
+          <h1>COUNTRY:{country.name.common}</h1>
+          <p>POPULATION: {country.population.toLocaleString()}</p>
+          <p>REGION:{country.region}</p>
+          <p>CAPITAL:{country.capital?.[0] || "Not Available"}</p>
+        </div>
+      </Link>
+    </div>
   );
 }
