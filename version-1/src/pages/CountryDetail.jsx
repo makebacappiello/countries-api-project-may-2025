6279d4270ad4a82127bbaeb3684eb77318bc2d8d
makebacappiello <makebacappiello@gmail.com>
2025-05-18 20:32:54 -0400

    added figma styling to css

--- version-1/src/pages/CountryDetail.jsx
+++ version-1/src/pages/CountryDetail.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 export default function CountryDetail({ apiData }) {
   const { countryName } = useParams();
+  console.log(countryName);
   const country = apiData.find(
     (c) => c.name.common.toLowerCase() === countryName.toLowerCase()
   );
@@ -17,17 +18,26 @@ export default function CountryDetail({ apiData }) {
   }
   return (
     <div className="country-detail">
-      <Link
-        to="/"
-        // {`country-detail/${country.name.common}`}
-      >
-        <div className="card">
-          <h1>Country Detail</h1>
-          <img src={country.flags.png} alt={`${country.name.common} flag`} />
-          <h1>COUNTRY:{country.name.common}</h1>
-          <p>POPULATION: {country.population.toLocaleString()}</p>
-          <p>REGION:{country.region}</p>
-          <p>CAPITAL:{country.capital?.[0] || "Not Available"}</p>
+      <Link to="/">
+        <div className="card country-detail-card">
+          <button className="back-button">&larr; Back</button>
+
+          <img
+            className="country-detail-flag"
+            src={country.flags.png}
+            alt={`${country.name.common} flag`}
+          />
+          <button className="save-button">Save</button>
+          <div className="country-detail-text">
+            <h2>{country.name.common}</h2>
+            <div className="detail-columns">
+              <div className="column">
+                <p>POPULATION: {country.population.toLocaleString()}</p>
+                <p>REGION:{country.region}</p>
+                <p>CAPITAL:{country.capital?.[0] || "No Capital Found"}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Link>
     </div>
